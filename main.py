b12b5378b8ff7633275862736aa3b104200d5672
Pedro Cobalquini <146349361+pedrocobalquini@users.noreply.github.com>
2026-05-23 03:33:12 -0300

    arquivo main add

---
+++ main.py
@@ -0,0 +1,27 @@
+from src.extract import get_link_url
+from src.transform import transform_data
+from src.load import load_to_supabase
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def run_etl():
+    print("--- Iniciando Pipeline ELT Pix ---")
+
+    dados_brutos = get_link_url()
+    
+    if dados_brutos:
+        print("Transformando dados...")
+        df_final = transform_data(dados_brutos)
+        print("Tratamento finalizado")
+        if not df_final.empty:
+            print("Iniciando no SUPABASE")
+            load_to_supabase(df_final)
+            print("--- Pipeline finalizada ---")
+        else:
+            print("Transformação resultou em DataFrame vazio")
+    else:
+        print("Falha na extração.")
+
+if __name__ == "__main__":
+    run_etl()
